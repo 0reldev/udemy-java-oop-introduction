a290df97936cbb30109a2dcbf8a99ac298fb3751
Aurélien Lonni <aurelien.lonni@gmail.com>
2023-03-16 14:48:29 +0100

    Creation of a Car class.

---
+++ src/main/java/Car.java
@@ -0,0 +1,16 @@
+public class Car {
+
+    private String make;
+    private String model;
+    private String color;
+    private int doors;
+    private boolean convertible;
+
+    public void describeCare() {
+        System.out.println(doors + "-Door "
+                + color + " "
+                + make + " "
+                + model + " "
+                + (convertible ? "Convertible" : ""));
+    }
+}
