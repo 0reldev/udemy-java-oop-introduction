3de40d70401ce2745e7932b0c5d5530e19c8b775
Aurélien Lonni <aurelien.lonni@gmail.com>
2023-03-20 17:55:32 +0100

    Lesson about getters in Java objects

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,10 +1,30 @@
 public class Car {
 
-    private String make;
-    private String model;
-    private String color;
-    private int doors;
-    private boolean convertible;
+    private String make = "Tesla";
+    private String model = "Model X";
+    private String color = "red";
+    private int doors = 3;
+    private boolean convertible = true;
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public boolean isConvertible() {
+        return convertible;
+    }
 
     public void describeCare() {
         System.out.println(doors + "-Door "
@@ -13,6 +33,4 @@ public class Car {
                 + model + " "
                 + (convertible ? "Convertible" : ""));
     }
-
-    //test
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-
+        Car car = new Car();
+    //    car.make = "Porshe";
+    //   car.model = "Carrera";
+     //   car.color = "blue";
+        System.out.println("make = " + car.getMake());
+        System.out.println("model = " + car.getModel());
+        car.describeCare();
     }
 }
