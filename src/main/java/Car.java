74f41acdd22031e2e972809f8f282dc3c25a47b0
Aurélien Lonni <aurelien.lonni@gmail.com>
2023-03-21 17:30:25 +0100

    Lesson about field  setters.

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -26,6 +26,33 @@ public class Car {
         return convertible;
     }
 
+    public void setMake(String make) {
+        if (make == null) make = "Unknown";
+        String lowercaseMake = make.toLowerCase();
+        switch (lowercaseMake) {
+            case "holden", "porsche", "tesla" -> this.make = make;
+            default -> {
+                this.make = "Unsupported";
+            }
+        }
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public void setConvertible(boolean convertible) {
+        this.convertible = convertible;
+    }
+
     public void describeCare() {
         System.out.println(doors + "-Door "
                 + color + " "
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,21 @@ public class Main {
     public static void main(String[] args) {
 
         Car car = new Car();
-    //    car.make = "Porshe";
-    //   car.model = "Carrera";
-     //   car.color = "blue";
+        car.setMake("Maserati");
+        car.setModel("Carrera");
+        car.setDoors(2);
+        car.setConvertible(true);
+        car.setColor("black");
         System.out.println("make = " + car.getMake());
         System.out.println("model = " + car.getModel());
         car.describeCare();
+
+        Car targa = new Car();
+        targa.setMake("Dodoche");
+        targa.setModel("DX");
+        targa.setDoors(5);
+        targa.setConvertible(true);
+        targa.setColor("blue");
+        targa.describeCare();
     }
 }
